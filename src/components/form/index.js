eda3aa88e7575e1172f0d915eacf2f0a6353f3cf
OmarHumamah <omar.nabeel.h@gmail.com>
2021-12-06 12:19:52 +0200

    l26

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,37 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import './form.scss';
+// import './form.scss';
 
-class Form extends React.Component {
+// class Form extends React.Component {
+// handleSubmit = (e) => {
+//   e.preventDefault();
+//   const formData = {
+//     method: "GET",
+//     url: "https://pokeapi.co/api/v2/pokemon"
+//   };
+//   this.props.handleApiCall(formData);
+// };
 
-  handleSubmit = e => {
+//   render() {
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             <span>URL: </span>
+//             <input name="url" type="text" />
+//             <button type="submit">GO!</button>
+//           </label>
+//           <label className="methods">
+//             <span id="get">GET</span>
+//             <span id="post">POST</span>
+//             <span id="put">PUT</span>
+//             <span id="delete">DELETE</span>
+//           </label>
+//         </form>
+//       </>
+//     );
+//   }
+// }
+
+// export default Form;
+
+export default function Form(props) {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: "GET",
+      url: "https://pokeapi.co/api/v2/pokemon"
     };
-    this.props.handleApiCall(formData);
-  }
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
+    props.handleApiCall(formData);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span class="url">URL: </span>
+          <input name="url" type="text" />
+          <button class="formbtn" type="submit">
+            GO!
+          </button>
+        </label>
+        <label className="methods">
+          <span id="get">GET</span>
+          <span id="post">POST</span>
+          <span id="put">PUT</span>
+          <span id="delete">DELETE</span>
+        </label>
+      </form>
+    </>
+  );
 }
-
-export default Form;
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+// class Results extends React.Component {
+//   render() {
+//     return (
+// <section>
+//   <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+// </section>
+//     );
+//   }
+// }
+
+// export default Results;
+
+export default function Results(props) {
+  return (
+    <>
+      <section id="results">
+        <pre>
+          {props.data ? JSON.stringify(props.data, undefined, 2) : null}
+        </pre>
       </section>
-    );
-  }
+    </>
+  );
 }
-
-export default Results;
