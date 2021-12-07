5e5ef98d2d009604d36b85bd8c9c5cc2679a4fda
OmarHumamah <omar.nabeel.h@gmail.com>
2021-12-07 21:20:49 +0200

    fix lab27

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -62,13 +62,17 @@ export default function Form(props) {
           </button>
         </label>
         <label className="methods">
-          <span onClick={()=>{setMethod('GET');}} data-testid='get' id="get">GET</span>
-          <span onClick={()=>{setMethod('POST');}} data-testid='post' id="post">POST</span>
-          <span onClick={()=>{setMethod('PUT');}} data-testid='put' id="put">PUT</span>
-          <span onClick={()=>{setMethod('DELETE');}} data-testid='delete' id="delete">DELETE</span>
+          <input type='radio' name='1' id="get" class="ch"/>
+          <label for="get" onClick={()=>{setMethod('GET');}} data-testid='get' id="get">GET</label>
+          <input type='radio' name='1' id="post" class="ch" />
+          <label for="post" onClick={()=>{setMethod('POST');}} data-testid='post' id="post">POST</label>
+          <input type='radio' name='1' id="put" class="ch" />
+          <label for="put" onClick={()=>{setMethod('PUT');}} data-testid='put' id="put">PUT</label>
+          <input type='radio' name='1' id="delete" class="ch" />
+          <label for="delete" onClick={()=>{setMethod('DELETE');}} data-testid='delete' id="delete">DELETE</label>
         </label>
         <h3>Body:</h3>
-        <textarea data-testid="body" onChange={(e)=>{setBody(e.target.value)}} name="body" type="text" rows='5'/>
+        <textarea id="area" data-testid="body" onChange={(e)=>{setBody(e.target.value)}} name="body" type="text" rows='5'/>
       </form>
     </>
   );
